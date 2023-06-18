27994b196488ff05b666b7136b4787eed95782fc
seoyeonko <seoyeon99o@naver.com>
2023-06-18 16:45:28 +0900

    study: use multiple env files

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -4,8 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 
+console.log('🛠️ env: ' + process.env.NODE_ENV); // print env variable
+console.log('🗂️ current working directory: ' + process.cwd()); // print current working directory
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+    }),
+    WeatherModule,
+  ],
   // isGlobal: ConfigModule 전역 설정 (else, 모든 파일에서 각각 import )
   controllers: [AppController],
   providers: [AppService],
