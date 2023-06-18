8b991ada536f4908cf99e1f6f23f0ee9b5aab8f0
seoyeonko <seoyeon99o@naver.com>
2023-06-18 17:12:15 +0900

    study: use yaml env file

--- chapter9/config-test/src/configs/config.ts
+++ chapter9/config-test/src/configs/config.ts
@@ -2,6 +2,8 @@ import common from './common';
 import local from './local';
 import dev from './dev';
 import prod from './prod';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
 
 const phase = process.env.NODE_ENV;
 
@@ -14,9 +16,14 @@ if (phase === 'local') {
   conf = prod;
 }
 
+const yamlConfig: Record<string, any> = yaml.load(
+  readFileSync(`${process.cwd()}/envs/config.yaml`, 'utf8'),
+);
+
 // 공통 환경 변수 반환
 // () => ({}): ConfigModule load 옵션 값의 형태
 export default () => ({
   ...common,
   ...conf,
+  ...yamlConfig,
 });
