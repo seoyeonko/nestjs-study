352bafde26b6a1b94b1e2b9098e5747c662e76ba
synk2577 <tjdus2577@gmail.com>
2023-08-02 23:28:51 +0900

    feat: comment create, delete api

--- chapter7/board/app.js
+++ chapter7/board/app.js
@@ -115,6 +115,64 @@ app.delete("/delete", async (req, res) => {
   }
 });
 
+// 댓글 추가// 댓글 추가
+app.post("/write-comment", async (req, res) => {
+  const { id, name, password, comment } = req.body; // body에서 데이터를 가지고 오기
+  const post = await postService.getPostById(collection, id); // id로 게시글의 정보를 가져오기
+  console.log(post);
+  // 게시글에 기존 댓글 리스트가 있으면 추가
+  if (post.comments) {
+    post.comments.push({
+      idx: post.comments.length + 1,
+      name,
+      password,
+      comment,
+      createdDt: new Date().toISOString(),
+    });
+  } else {
+    // 게시글에 댓글 정보가 없으면 리스트에 댓글 정보 추가
+    post.comments = [
+      {
+        idx: 1,
+        name,
+        password,
+        comment,
+        createdDt: new Date().toISOString(),
+      },
+    ];
+  }
+
+  // 업데이트하기. 업데이트 후에는 상세페이지로 다시 리다이렉트
+  postService.updatePost(collection, id, post);
+  return res.redirect(`/detail/${id}`);
+});
+
+// 댓글 삭제
+app.delete("/delete-comment", async (req, res) => {
+  const { id, idx, password } = req.body;
+  console.log(id, idx, password);
+
+  // 게시글(post)의 comments 안에 있는 특정 댓글 데이터를 찾기
+  const post = await collection.findOne(
+    {
+      _id: ObjectId(id),
+      comments: { $elemMatch: { idx: parseInt(idx), password } },
+    },
+    postService.projectionOption
+  );
+  console.log('post >>>', post)
+
+  // 데이터가 없으면 isSuccess : false를 주면서 종료
+  if (!post) {
+    return res.json({ isSuccess: false });
+  }
+
+  // 댓글 번호가 idx 이외인 것만 comments에 다시 할당 후 저장
+  post.comments = post.comments.filter((comment) => comment.idx != idx);
+  postService.updatePost(collection, id, post);
+  return res.json({ isSuccess: true });
+});
+
 let collection;
 app.listen(3000, async () => {
   console.log('server started');
